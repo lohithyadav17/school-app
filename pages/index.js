6da12ce7e76a113bc72afca226e0262d6b57e19d
lohithyadav17 <114151353+lohithyadav17@users.noreply.github.com>
2025-09-04 21:13:32 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
-import Link from "next/link"; 
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function Home() {
   const [cities, setCities] = useState([]);
